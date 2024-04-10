4c276f7b06c4c52ffdc30ee593004a02d9bdddfd
Matthieu Serek <matthieu.serek@bpm-conseil.com>
2024-04-10 16:27:12 +0200

    Exercise 7 et 9

--- src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
+++ src/main/java/fr/polytech/sim/cycling/BikeSimulator.java
@@ -13,6 +13,17 @@ import fr.polytech.sim.utils.Context;
 public class BikeSimulator implements Simulation {
     private final Logger logger = new TimestampedLoggerDecorator(LoggerCreator.getLoggerCreator().create("BikeSimulator"));
 
+//    public void run() {
+//        Bike bike = Context.inject(Bike.class);
+//        if(bike != null){
+//            this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
+//            this.logger.log("Bike's mass %.2f Kg.", bike.getMass());
+//        } else {
+//            this.logger.log("Injection failed.");
+//        }
+//    }
+
+    // exercice 9 :
     public void run() {
         for (Bike bike : Context.injectAll(Bike.class)) {
             this.logger.log("Bike's speed %.2f Km/h.", bike.getVelocity());
